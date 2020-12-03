a3edecc8561715595d76ec32569c18e5de583ca1
Excellent_Torch <ishanmaduranga42@gmail.com>
2020-12-04 04:25:13 +0530

    Update Covid-Torch.py
    Updated the script to python3

--- Covid-Torch.py
+++ Covid-Torch.py
@@ -7,15 +7,15 @@ from termcolor import cprint
 covid19_api = requests.get('https://api.kawalcorona.com/').text
 countries = re.findall('"Country_Region":"(.*?)"', covid19_api)
 
-print '\n\x1b[0;37m========================================================\x1b[0;37m'
+print('\n\x1b[0;37m========================================================\x1b[0;37m')
 
 cprint(figlet_format("Covid-Torch", font = "doom"),"blue")
 
-print '\n\x1b[0;37m[ --__-- ]===>[\x1b[40;32mForged By Excellent_Torch\x1b[0;37m]<===[ --__-- ]'
-print '\n\x1b[0;37m========================================================\x1b[0;37m'
+print('\n\x1b[0;37m[ --__-- ]===>[\x1b[40;32mForged By Excellent_Torch\x1b[0;37m]<===[ --__-- ]')
+print('\n\x1b[0;37m========================================================\x1b[0;37m')
 
 
-ask = raw_input('\n\x1b[44;37m=>[ \x1b[1;32mEnter Your Country \x1b[0;37m] := ')
+ask = input('\n\x1b[44;37m=>[ \x1b[1;32mEnter Your Country \x1b[0;37m] := ')
 
 cry = ('').join(gcm(ask, countries, n=1, cutoff=0))
 
@@ -23,11 +23,8 @@ info = '{"OBJECTID":.*?,"Country_Region":"' + cry + '","Last_Update":.*?,"Lat":.
 
 results = re.search(info, covid19_api)
 
-print ('\n\n\x1b[0;37m[ + ]============[\x1b[1;32m{}\x1b[0;37m] [\x1b[1;32mStatus\x1b[0;37m]=======[ + ]').format(cry)
-print ('\n             [ \x1b[1;32mInfected \x1b[0;37m] : [\x1b[1;32m{}\x1b[0;37m]\n             \x1b[0;37m[ \x1b[1;32mDeaths \x1b[0;37m] : [\x1b[1;32m{}\x1b[0;37m]\n             \x1b[0;37m[ \x1b[1;32mRecovered \x1b[0;37m] : [\x1b[1;32m{}\x1b[0;37m]\n             \x1b[0;37m[ \x1b[1;32mActive \x1b[0;37m] : [\x1b[1;32m{}\x1b[0;37m]\n').format(results.group(1), results.group(2), results.group(3), results.group(4))
-print '\x1b[0;37m[ + ]======================================[ + ]\x1b[0;37m'
-
-
-
+print(('\n\n\x1b[0;37m[ + ]============[\x1b[1;32m{}\x1b[0;37m] [\x1b[1;32mStatus\x1b[0;37m]=======[ + ]').format(cry))
+print(('\n             [ \x1b[1;32mInfected \x1b[0;37m] : [\x1b[1;32m{}\x1b[0;37m]\n             \x1b[0;37m[ \x1b[1;32mDeaths \x1b[0;37m] : [\x1b[1;32m{}\x1b[0;37m]\n             \x1b[0;37m[ \x1b[1;32mRecovered \x1b[0;37m] : [\x1b[1;32m{}\x1b[0;37m]\n             \x1b[0;37m[ \x1b[1;32mActive \x1b[0;37m] : [\x1b[1;32m{}\x1b[0;37m]\n').format(results.group(1), results.group(2), results.group(3), results.group(4)))
+print('\x1b[0;37m[ + ]======================================[ + ]\x1b[0;37m')
 
 
